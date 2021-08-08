9658f4cb5c445d1dff08004621ac4be381baaee2
ShadiestGoat <shady@shadygoat.eu>
2021-08-08 16:01:29 +0300

    streak point calculations... i forgot to ADD

--- src/Components/calsses.ts
+++ src/Components/calsses.ts
@@ -97,7 +97,7 @@ class userMgr {
         if (correct == "yes") {
             this.cache[user].right++
             this.cache[user].streak++
-            this.cache[user].streakP = this.steakPointCalc(this.cache[user].streak)
+            this.cache[user].streakP += this.steakPointCalc(this.cache[user].streak)
             if (this.cache[user].scores.streak < this.cache[user].streak) this.cache[user].scores.streak = this.cache[user].streak //i think this is faster than ++
         } else if (correct == "no") {
             this.cache[user].wrong++
